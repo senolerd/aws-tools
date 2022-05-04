3024cb820603e1fd8cb3b8f359d38d287e52428d
Senol E <whereimdb@gmail.com>
2022-05-04 15:23:04 -0500

    1

--- ec2info.sh
+++ ec2info.sh
@@ -11,68 +11,82 @@ color="$c1$c2$c3"
 bg-color="$c3$c2$c1"
 # [ec2-user@ip-172-31-12-137 ~]$ for item in $(curl 169.254.169.254/latest/meta-data); do echo $item ==> $(curl  169.254.169.254/latest/meta-data/$item); done
 
-ami-id
-ami-launch-index
-ami-manifest-path
-autoscaling/
-block-device-mapping/
-events/
-hostname
-identity-credentials/
-instance-action
-instance-id
-instance-life-cycle
-instance-type
-local-hostname
-local-ipv4
-mac
-metrics/
-network/
-placement/
-profile
-public-hostname
-public-ipv4
-public-keys/
-reservation-id
-security-groups
+# ami-id
+# ami-launch-index
+# ami-manifest-path
+# autoscaling/
+# block-device-mapping/
+# events/
+# hostname .
+# identity-credentials/
+# instance-action
+# instance-id
+# instance-life-cycle
+# instance-type
+# local-hostname .
+# local-ipv4
+# mac
+# metrics/
+# network/
+# placement/
+# profile
+# public-hostname .
+# public-ipv4 .
+# public-keys/
+# reservation-id
+# security-groups
+
+
+metalist=(
+    "hostname" \
+    "instance-id" \
+    "local-hostname" \
+    "local-ipv4" \
+    "public-hostname" \
+    "public-ipv4" \    
+    )
+
+
+
+for label in ${metalist[@]}
+    do
+        echo $label
+    done
 
 
 
 
 cat << EoF
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
 
-  <body>
-      <div class="main">
-        
+        <body>
+            <div class="main">
+                
 
-      </div>
+            </div>
 
-      <style>
-          :root { --color: $color; --bgcolor: $bg-color }
-          .main { color: var(--color); background-color: var(--bgcolor);  }
-          .label { color: var(--color) }
-          .value { color: var(--color) }
-      </style>
+            <style>
+                :root { --color: $color; --bgcolor: $bg-color }
+                .main { color: var(--color); background-color: var(--bgcolor);  }
+                .label { color: var(--color) }
+                .value { color: var(--color) }
+            </style>
 
-  </body>
+        </body>
 
-</html>
+        </html>
 
-
-
-
-</body>
-</html>
+        </body>
+    </html>
 
 
 EoF
